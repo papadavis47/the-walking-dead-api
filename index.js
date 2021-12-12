64f3dcbf75089b7e096b0fbd0e8280085152a181
John William Davis <jwdavisdeveloper@gmail.com>
2021-12-12 02:26:47 -0800

    Setup static home page, static docs page

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 const express = require("express");
+const path = require("path");
 const { characters } = require("./data.js");
 
 const app = express();
 
 const PORT = 5001;
 
+app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send('<h1>The Walking Dead REST API</h1><a href="/api/characters">Characters<a/>');
-});
-
-app.get("/api/characters", (req, res) => {
+app.get("/api/v1/characters", (req, res) => {
   res.json(characters);
 });
 
-app.get("/api/characters/:characterId", (req, res) => {
+app.get("/api/v1/characters/:characterId", (req, res) => {
   const { characterId } = req.params;
   const singleCharacter = characters.find((character) => character.id === Number(characterId));
   if (!singleCharacter) {
     res.status(404).send("Character not available");
   }
   return res.json(singleCharacter);
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("Requested Resource Not Found");
 });
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}. Oh Yeah Baby!`));
